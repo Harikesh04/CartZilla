a5fb70ace43f6a087ee809f23dd3970641423163
Harry <harikeshkrsingh20@gmail.com>
2023-12-24 23:14:43 +0530

    fixed cors issue

--- backend/app.js
+++ backend/app.js
@@ -28,7 +28,7 @@ app.use(bodyParser.urlencoded({ extended: true })); //
 app.use(fileUpload());
 app.use(
   cors({
-    origin: '*',
+    origin: ['http://localhost:3000','https://cartzilla-mauve.vercel.app'],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
